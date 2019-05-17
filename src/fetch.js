918cd20f9b96f8c98813eb48e219facf45d0e94c
sup.me <1035924544@qq.com>
2019-05-17 10:53:08 +0800

    token

--- src/fetch.js
+++ src/fetch.js
@@ -7,15 +7,17 @@ const service = axios.create({
     baseURL,
     timeout: 5000
 });
-
-const Tokena = "6"
-const Tokenb = "a4e4e5e47b244d93b7c0ca1c8b9a5994ec60324"
+const Tokena = "87273968"
+const Tokenb = "fac550a789c"
+const Token = "512a55b72"
+const T = "ee051541"
+const F = "f4fd"
 
 /* 请求拦截器 */
 service.interceptors.request.use( config => {
     config.headers = { // 设置请求头
         'Content-Type': 'application/json',
-        'Authorization': " bearer " + Tokena + Tokenb + " "
+        'Authorization': " bearer " + Tokena + Tokenb + Token + T + F + " "
     }
     return config;
 })
